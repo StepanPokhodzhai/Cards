acc6583efd61204f9a30fee8943dc79c56faa1ca
StepanPokhodzhai <stelkerstepan@gmail.com>
2023-01-01 21:28:11 +0200

    1st commit
    1st commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+	//cards := newDeckFromFile("my_card")
+	//cards.print()
+	//cards := newDeck()
+	//cards.saveToFile("my_cards")
+	//hand, remainingCards := deal(cards, 5)
+	//hand.print()
+	//remainingCards.print()
+	//cards.print()
+	//greeting := "Hi there!"
+	//fmt.Println([]byte(greeting))
+	//fmt.Println(cards.toString())
+}
